4341942ee6738a679ffe7527964101d0766129b1
Matteo Ricupero <info@matteoricupero.it>
2024-11-15 18:06:26 +0100

    base name

--- src/sample.config.ts
+++ src/sample.config.ts
@@ -1,9 +1,9 @@
 export const config = {
-  name: "WoodyBooth",
-  email: "info@woodybooth.it",
+  name: "TT Club Manager",
+  email: "info@matteoricupero.it",
   apple: {
-    issuerId: "f311caa5-3380-4287-97b4-a19ee3ce56cd",
-    keyId: "63T65XFP9S",
-    bundleId: "it.matteoricupero.woodyboothexpress",
+    issuerId: "",
+    keyId: "",
+    bundleId: "",
   },
 };
