2f7dc40d07aebb89cb3f5ffa2a245b8a700653a0
Matteo Ricupero <info@matteoricupero.it>
2024-12-01 10:16:21 +0100

    delete account request

--- src/controller/user/me.controller.ts
+++ src/controller/user/me.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { extensionSendTelegramBotMessage } from "../../extensions/telegram.extension";
 import { UserService } from "../../services/user.service";
 
 export const meHandler = async (req: Request, res: Response) => {
@@ -15,6 +16,39 @@ export const meHandler = async (req: Request, res: Response) => {
     res.status(500).jsonTyped({
       status: "error",
       message: "Failed to get user",
+      data: null,
+    });
+  }
+};
+
+export const deleteMeHandler = async (req: Request, res: Response) => {
+  const idUser = req.headers.id_user as string;
+
+  try {
+    const user = await UserService.getById(idUser);
+
+    if (!user) {
+      res.status(404).jsonTyped({
+        status: "error",
+        message: "User not found",
+        data: null,
+      });
+    }
+
+    await extensionSendTelegramBotMessage({
+      content: `User account deletion request: ${idUser} ${user.email}`,
+      parse_mode: "HTML",
+    });
+
+    res.status(200).jsonTyped({
+      status: "success",
+      message: "User account deletion requested",
+      data: null,
+    });
+  } catch (error) {
+    res.status(500).jsonTyped({
+      status: "error",
+      message: "Failed to delete user",
       data: null,
     });
   }
