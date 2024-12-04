2efa2dfa97103d4bd7e5792e0c9ae7e9937ee4d3
Matteo Ricupero <info@matteoricupero.it>
2024-12-04 15:25:42 +0100

    allow cookies on development (safari fix)

--- src/libs/cookie.ts
+++ src/libs/cookie.ts
@@ -8,37 +8,41 @@ export const setCookies = (
   refreshToken: string,
   res: Response
 ) => {
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("access_token", accessToken, {
-    domain: process.env.DOMAIN,
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    domain: isProduction ? process.env.DOMAIN : undefined,
+    httpOnly: isProduction,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
     maxAge: JWT_EXPIRATION_TIME,
   });
 
   res.cookie("refresh_token", refreshToken, {
-    domain: process.env.DOMAIN,
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    domain: isProduction ? process.env.DOMAIN : undefined,
+    httpOnly: isProduction,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
     maxAge: REFRESH_TOKEN_EXPIRATION_TIME,
   });
 };
 
 export const clearCookies = (res: Response) => {
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("access_token", "", {
-    domain: process.env.DOMAIN,
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    domain: isProduction ? process.env.DOMAIN : undefined,
+    httpOnly: isProduction,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
     expires: new Date(0),
   });
 
   res.cookie("refresh_token", "", {
-    domain: process.env.DOMAIN,
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    domain: isProduction ? process.env.DOMAIN : undefined,
+    httpOnly: isProduction,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
     expires: new Date(0),
   });
 };
